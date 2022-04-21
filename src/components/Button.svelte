10b860db4564709afcec7a27e820dd8491b07e4f
therealpaulgg <paul@paul.systems>
2022-04-20 23:45:29 -0700

    dark mode is no longer optional, enhances performance

--- src/components/Button.svelte
+++ src/components/Button.svelte
@@ -12,15 +12,15 @@
     {type}
     {disabled}
     class={`${
-        active ? "bg-custom-light-pink text-black" : invertColor ? "dark:bg-custom-titanium bg-gray-300" : "bg-custom-titanium dark:bg-gray-300"
-    } ${invertColor ? 'text-black dark:text-white' : 'text-white dark:text-black'}   shadow text-sm transition duration-200 m-1 btn ${
+        active ? "bg-custom-light-pink text-black" : invertColor ? "bg-gray-300" : "bg-custom-titanium"
+    } ${invertColor ? 'text-black' : 'text-white'}   shadow text-sm transition duration-200 m-1 btn ${
         uppercase ? "uppercase" : ""
     } py-2 px-4 ${
         circular ? "w-12 h-12 rounded-full flex items-center justify-center" : "rounded "
     } ${
         disabled
             ? "opacity-50 cursor-not-allowed "
-            : "hover:bg-custom-light-pink dark:hover:text-white dark:hover:bg-custom-light-pink hover:text-white "
+            : "hover:bg-custom-light-pink hover:text-white "
     }`}
 >
     <slot />
--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,10 +1,9 @@
 <script lang="ts">
     import { page } from "$app/stores";
     import dayjs from "dayjs";
-    import { setTheme, theme } from "../store";
 
     import "../app.css";
-    import Navbar from "../components/Navbar.svelte"
+    import Navbar from "../components/Navbar.svelte";
     import Typewriter from "../components/Typewriter.svelte";
     import Icon from "svelte-awesome/components/Icon.svelte";
     import Button from "../components/Button.svelte";
@@ -18,47 +17,30 @@
     />
 </svelte:head>
 
-<div class={`${$theme === "dark" ? "dark" : ""}`}>
-    <div class={`relative min-h-screen dark:bg-custom-dark-theme dark:text-white font-body pb-16 `}>
-        <Navbar>
-            <div slot="name">
-                <div>
-                    <div class="pr-4">Paul Gellai Portfolio</div>
-                    <div class="text-base">
-                        <Typewriter
-                            words={["Hack", "The", "Planet", "🌎"]}
-                            next-word-interval={100}
-                        />
-                    </div>
+<div class={`relative min-h-screen bg-custom-dark-theme text-white font-body pb-16 `}>
+    <Navbar>
+        <div slot="name">
+            <div>
+                <div class="pr-4">Paul Gellai Portfolio</div>
+                <div class="text-base">
+                    <Typewriter words={["Hack", "The", "Planet", "🌎"]} next-word-interval={100} />
                 </div>
             </div>
-            <div slot="links" class="flex gap-4">
-                <a href="/" class="navbar-link" class:active={$page.url.pathname === "/"}
-                        >Home</a
-                    >
-                    <a
-                        href="/portfolio"
-                        class="navbar-link"
-                        class:active={$page.url.pathname === "/portfolio"}>Portfolio</a
-                    >
-                    <a
-                        href="/skills"
-                        class="navbar-link"
-                        class:active={$page.url.pathname === "/skills"}>Skills</a
-                    >
-                    <a href="https://blog.paulgellai.dev" class="navbar-link">Blog</a>
-            </div>
-            <div slot="right">
-                <Button invertColor circular on:click={() => setTheme($theme === "light" ? "dark" : "light")}>
-                    {#if $theme === "dark"}
-                        <Icon data={faMoon} />
-                    {:else}
-                        <Icon data={faSun} />
-                    {/if}
-                </Button>
-            </div>
-        </Navbar>
-        <!-- <nav class="bg-custom-pink p-3 mb-3 text-white">
+        </div>
+        <div slot="links" class="flex gap-4">
+            <a href="/" class="navbar-link" class:active={$page.url.pathname === "/"}>Home</a>
+            <a
+                href="/portfolio"
+                class="navbar-link"
+                class:active={$page.url.pathname === "/portfolio"}>Portfolio</a
+            >
+            <a href="/skills" class="navbar-link" class:active={$page.url.pathname === "/skills"}
+                >Skills</a
+            >
+            <a href="https://blog.paulgellai.dev" class="navbar-link">Blog</a>
+        </div>
+    </Navbar>
+    <!-- <nav class="bg-custom-pink p-3 mb-3 text-white">
             <div class="mx-auto container flex items-center justify-between">
                 <div class="flex items-center gap-6 text-lg">
                     
@@ -68,16 +50,13 @@
             </div>
         </nav> -->
 
-        <div class="mx-auto container pb-16 px-2"><slot /></div>
-        <footer
-            class="bottom-0 left-0 right-0 p-3 absolute w-full bg-gray-200 dark:bg-custom-titanium"
-        >
-            <div class="mx-auto container text-center">
-                Copyright © {dayjs().year()} <a href="https://paulgellai.dev">Paul Gellai.</a> All rights
-                reserved.
-            </div>
-        </footer>
-    </div>
+    <div class="mx-auto container pb-16 px-2"><slot /></div>
+    <footer class="bottom-0 left-0 right-0 p-3 absolute w-full bg-custom-titanium">
+        <div class="mx-auto container text-center">
+            Copyright © {dayjs().year()} <a href="https://paulgellai.dev">Paul Gellai.</a> All rights
+            reserved.
+        </div>
+    </footer>
 </div>
 
 <style>
